f0701b920b19f1eea48947f5db7314bc328eddc4
Sergei Voitenko <voitenko.sv@gmail.com>
2015-10-02 20:58:59 +0300

    Changed settings to work on MAC

--- src/main/java/core/ApplicationSettings.java
+++ src/main/java/core/ApplicationSettings.java
@@ -4,7 +4,7 @@ package core;
  * Created by vbalaban on 2/23/2015.
  */
 public class ApplicationSettings {
-    public static String baseUrl = "localhost:25774//wp-admin";
-    public static String username = "admin ";
+    public static String baseUrl = "http://localhost/blog/wp-login.php";
+    public static String username = "admin";
     public static String password = "admin";
 }
